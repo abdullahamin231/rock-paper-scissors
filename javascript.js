90fb600c04ba567682d3b777c6a6be5d6cb6e902
Abdullah Amin <abdullahamin231@gmail.com>
2023-09-06 21:18:57 +0500

    final changed made, loops remaining

--- javascript.js
+++ javascript.js
@@ -1,29 +1,31 @@
 function getComputerChoice(){
-choices = ["rock", "paper", "scissors"];
-return choices[Math.floor(Math.random() * choices.length)];
-}
-
-function playerChoice(){ 
-    return prompt("Enter your choice: ");
-}
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-      return "It's a draw!";
-    } else if (
-      (playerSelection === "rock" && computerSelection === "scissors") ||
-      (playerSelection === "paper" && computerSelection === "rock") ||
-      (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-      return "You win! " + playerSelection + " beats " + computerSelection;
-    } else {
-      return "You lose! " + computerSelection + " beats " + playerSelection;
-    }
+  choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
   }
-
-function run(){
-    comp = getComputerChoice();
-    player = playerChoice()
-    return playRound(player, comp);
-
-}
+  
+  function playerChoice(){ 
+      return prompt("Enter your choice: ");
+  }
+  var player = 0; var comp= 0;
+  function playRound(playerSelection, computerSelection) {
+      if (playerSelection === computerSelection) {
+        return "It's a draw!";
+      } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+      ) {
+        player++
+        return "You win! " + playerSelection + " beats " + computerSelection;
+      } else {
+        comp++
+        return "You lose! " + computerSelection + " beats " + playerSelection;
+      }
+    }
+  
+  function run(){
+      comp = getComputerChoice();
+      player = playerChoice()
+      return playRound(player, comp);
+  
+  }
