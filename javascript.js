86349a08a94daa08591db4747aeb4227b80f3132
Abdullah Amin <abdullahamin231@gmail.com>
2023-10-01 17:45:48 +0500

    done

--- javascript.js
+++ javascript.js
@@ -1,31 +1,37 @@
+var player = 0; var comp= 0;
+const buttons = document.querySelectorAll("button");
+
+
 function getComputerChoice(){
   choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
-  }
-  
-  function playerChoice(){ 
-      return prompt("Enter your choice: ");
-  }
-  var player = 0; var comp= 0;
-  function playRound(playerSelection, computerSelection) {
+}
+
+function playRound(playerSelection) {
+      let computerSelection = getComputerChoice();
+	  let result = "";
       if (playerSelection === computerSelection) {
-        return "It's a draw!";
+        result = "It's a draw!";
       } else if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
       ) {
-        player++
-        return "You win! " + playerSelection + " beats " + computerSelection;
+        player+=1;
+        result = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "<br><br>Player score: " + player + "<br>Computer score: " + comp)
       } else {
-        comp++
-        return "You lose! " + computerSelection + " beats " + playerSelection;
+        comp+=1;
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
       }
-    }
+	  document.getElementById('result').innerHTML = result
+    return
+
+}
+
+
+buttons.forEach(button => {
+	button.addEventListener('click', function() { playRound(button.value)})
+})
   
-  function run(){
-      comp = getComputerChoice();
-      player = playerChoice()
-      return playRound(player, comp);
-  
-  }
